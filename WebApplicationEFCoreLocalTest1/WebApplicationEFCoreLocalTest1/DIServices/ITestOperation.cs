d27e93b520ea79840b9b70bab66116db7181606d
VincentFang <463890315@qq.com>
2019-09-23 19:06:28 +0800

    测试依赖注入1

---
+++ WebApplicationEFCoreLocalTest1/WebApplicationEFCoreLocalTest1/DIServices/ITestOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationEFCoreLocalTest1.DIServices
+{
+    public interface ITestOperation
+    {
+        Guid OpId { get; }
+    }
+    //单例服务测试接口
+    public interface ITestOperationSingleton : ITestOperation
+    {
+    }
+    //临时服务测试接口
+    public interface ITestOperationTransient : ITestOperation
+    {
+    }
+    //范围服务测试接口
+    public interface ITestOperationScoped : ITestOperation
+    {
+    }
+
+    //依赖接口的具体实现
+    public class TestOperation : ITestOperationSingleton, ITestOperationTransient, ITestOperationScoped
+    {
+        private Guid _guid;
+        public Guid OpId => _guid;
+        public TestOperation()
+        {
+            this._guid = Guid.NewGuid();
+        }
+
+        public TestOperation(Guid guid)
+        {
+            this._guid = guid;
+        }
+    }
+}
--- WebApplicationEFCoreLocalTest1/WebApplicationEFCoreLocalTest1/Startup.cs
+++ WebApplicationEFCoreLocalTest1/WebApplicationEFCoreLocalTest1/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplicationEFCoreLocalTest1.Repositories;
 using WebApplicationEFCoreLocalTest1.Middlewares;
+using WebApplicationEFCoreLocalTest1.DIServices;
 
 namespace WebApplicationEFCoreLocalTest1
 {
@@ -38,6 +39,33 @@ namespace WebApplicationEFCoreLocalTest1
             //配置Repository注入
             services.AddTransient<UserRepository>();
             services.AddTransient<UserWithSqlRepository>();
+            #region 测试依赖注入及其生命周期
+            // 默认构造
+            services.AddSingleton<ITestOperationSingleton, TestOperation>();
+            //自定义传入Guid空值
+            services.AddSingleton<ITestOperationSingleton>(new TestOperation());
+            // 自定义传入一个New的Guid
+            services.AddSingleton<ITestOperationSingleton>(new TestOperation(Guid.Empty));
+            //Transient服务
+            services.AddTransient<ITestOperationTransient, TestOperation>();
+            //Scoped服务
+            services.AddScoped<ITestOperationScoped, TestOperation>();
+            //通过privider创建服务实例
+            var provider = services.BuildServiceProvider();
+            var testSingle1 = provider.GetService<ITestOperationSingleton>();
+            var testSingle2 = provider.GetService<ITestOperationSingleton>();
+            var testSingle3 = provider.GetService<ITestOperationSingleton>();
+            var testTransient1 = provider.GetService<ITestOperationTransient>();
+            var testTransient2 = provider.GetService<ITestOperationTransient>();
+            var testTransient3 = provider.GetService<ITestOperationTransient>();
+            var testScoped1 = provider.GetService<ITestOperationScoped>();
+            var testScoped2 = provider.GetService<ITestOperationScoped>();
+            using (var scope2 = provider.CreateScope())
+            {
+                var provider2 = scope2.ServiceProvider;
+                var testScoped3 = provider2.GetService<ITestOperationScoped>();
+            }
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
